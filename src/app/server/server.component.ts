3c7245bb5fb37bd8e16d56fc61913bf3be92da3f
willys_tjiawi <willys@huubap.com>
2018-05-18 13:24:59 +0800

    ngStyle ngClass

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,10 +2,29 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
+  styles: [`
+    .online {
+        color: white;
+        margin: 2px 0px;
+      }
+    .offline {
+        color: aqua;
+        margin: 2px 0px;
+    }
+    `]
 })
 
 export class ServerComponent {
   serverId = 108;
-  serverStatus = 'Online';
+  serverStatus: string;
+
+  constructor () {
+    this.serverStatus = (Math.random() * 5 < 2) ? 'Offline' : 'Online';
+  }
+
+  setServerStatusColor () {
+    return (this.serverStatus === 'Online') ? '#018786' : '#B00020';
+  }
+
 }
