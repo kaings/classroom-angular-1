32a6519cd75368f4a0fec68e2b949f4f28b817ab
willys_tjiawi <willys@huubap.com>
2018-05-18 09:33:40 +0800

    ngif example

--- src/app/externalserver/externalserver.component.ts
+++ src/app/externalserver/externalserver.component.ts
@@ -3,9 +3,31 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-externalserver',
-  templateUrl: './externalserver.component.html'
+  templateUrl: './externalserver.component.html',
+  styles: [
+  `button {
+      margin: 10px 20px;
+  }
+  `]
 })
 
 export class ExternalserverComponent {
+
+  createExternalServer = false;
+  externalServerInput: string;
+
+  constructor () {}
+
+  onExternalServerInput (event: Event) {
+    this.externalServerInput = (<HTMLInputElement>event.target).value;
+  }
+
+  onCreateExternalServer () {
+    this.createExternalServer = true;
+  }
+
+  onFocusLetsClear () {
+    this.createExternalServer = false;
+  }
 
 }
